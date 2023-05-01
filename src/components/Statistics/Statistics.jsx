ae9b992c85425ebd0193d3fd04852f82bb56a665
Serhii Pradid <115069705+Serhii-Pradid@users.noreply.github.com>
2023-05-01 23:00:20 +0300

    task 2 added style

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
 
 export const Statistic = ({ title, stats }) => {
    
     return (
-        <section className="statistics">
-  {title && <h2 className="title">{title}</h2>}
+        <section className={css.statistics}>
+  {title && <h2 className={css.title}>{title}</h2>}
 
-  <ul className="stat-list">
+  <ul className={css.statList}>
     {stats.map(({ id, label, percentage }) => {
     return(
         <li 
-        className="item" 
+        className={css.item} 
         key={id}
         >
-      <span className="label">{label}</span>
-      <span className="percentage">{percentage}</span>
+      <span className={css.label}>{label}</span>
+      <span className={css.percentage}>{percentage}</span>
       </li>
     );
 })}
